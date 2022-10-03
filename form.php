4ce503d332730db6cf44b7956a84faf101bd4049
Omar Lahouaichri <lahouaichri.omar@gmail.com>
2022-10-03 16:56:30 +0200

    first edition

--- form.php
+++ form.php
@@ -1,30 +1,46 @@
+<?php
+$errors = [];
+?>
 <form action="thanks.php" method="post">
     <div>
         <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_name">
+        <input type="text" id="nom" name="user_name" required>
     </div>
     <div>
         <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="user_firstname">
+        <input type="text" id="prenom" name="user_firstname" required>
     </div>
     <div>
         <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
+        <input type="email" id="courriel" name="user_email" required>
     </div>
     <div>
-        <label for="telephone">Nom :</label>
-        <input type="tel" id="telephone" name="user_phone">
+        <label for="telephone">Téléphone :</label>
+        <input type="tel" id="telephone" name="user_phone" required>
     </div>
-    <select name="topic">
+    <select name="topic" required>
         <option value="demande d'aide">besoin d'aide</option>
         <option value="demande de conseils">besoin de conseils</option>
         <option value="demande de rendez-vous">besoin d'un rendez-vous</option>
     </select>
     <div>
         <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
+        <textarea id="message" name="user_message" required></textarea>
     </div>
     <div class="button">
         <button type="submit">Envoyer votre message</button>
     </div>
+    <div>
+        <?php // Affichage des éventuelles erreurs 
+        if (count($errors) > 0) : ?>
+            <div class="border border-danger rounded p-3 m-5 bg-danger">
+                <ul>
+                    <?php foreach ($errors as $error) : ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+    </div>
+
 </form>
--- thanks.php
+++ thanks.php
@@ -1,5 +1,44 @@
-<h1>Merci <?= $_POST["user_name"] . " " . $_POST["user_firstname"] ?> de nous avoir contacté à propos de votre <?= $_POST["topic"] ?>.
+<?php
+$errors = [];
+$validate = "Merci "  . $_POST["user_name"] . " " . $_POST["user_firstname"] . " de nous avoir contacté à propos de votre " . $_POST["topic"] . "." .
 
-    Un de nos conseiller vous contactera soit à l’adresse <?= $_POST["user_email"] ?> ou par téléphone au <?= $_POST["user_phone"] ?> dans les plus brefs délais pour traiter votre demande :
+    " Un de nos conseiller vous contactera soit à l’adresse " . $_POST["user_email"] . " ou par téléphone au " . $_POST["user_phone"] . " dans les plus brefs délais pour traiter votre demande : " .
 
-    <?= $_POST["user_message"] ?></h1>
+    $_POST["user_message"] . ".";
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (empty($_POST["user_name"]) || trim($_POST["user_name"]) === "") {
+        $errors[] = "Vous devez saisir votre nom.";
+    }
+    if (empty($_POST["user_firstname"]) || trim($_POST["user_firstname"]) === "") {
+        $errors[] = "Vous devez saisir votre prénom.";
+    }
+    if (empty($_POST["user_email"]) || trim($_POST["user_email"]) === "" || filter_var($_POST["user_email"], FILTER_VALIDATE_EMAIL) === false) {
+        $errors[] = "Vous devez entrer un email valide.";
+    }
+    if (empty($_POST["user_phone"]) || trim($_POST["user_phone"]) === "") {
+        $errors[] = "Vous devez saisir un numero de téléphone.";
+    }
+    if (empty($_POST["user_message"]) || trim($_POST["user_message"]) === "") {
+        $errors[] = "Vous devez entrer un message.";
+    }
+    if (count($errors) === 0) {
+        echo $validate;
+    }
+}
+
+?>
+
+
+
+<?php
+if (count($errors) > 0) : ?>
+    <div>
+        <ul>
+            <?php foreach ($errors as $error) : ?>
+                <li><?= $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
